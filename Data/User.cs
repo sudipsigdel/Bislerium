bfcdfe692e5d6ed6aafd2bef25393b48cac667d2
Sudip Sigdel <sudipsigdel2059@gmail.com>
2024-01-07 09:53:35 +0545

    Initial Commit

---
+++ Data/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bislerium.Data
+{
+    public class User
+    {
+        public string UserName { get; set; }
+
+        public Role Role { get; set; }
+
+        public string Password { get; set; }
+
+        public bool HasInitialPasswordChanged { get; set; } = false;
+    }
+}
---
+++ Data/UserServices.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bislerium.Data
+{
+    public class UserServices
+    {
+
+        private List<User> _seedUsersList = new()
+        {
+            new User()
+            {
+                Password = "admin",
+                Role = Role.Admin,
+            },
+
+            new User()
+            {
+                Password = "staff",
+                Role = Role.Staff
+            }
+        };
+
+        public User loginCheck(String Password)
+        {
+            User user = _seedUsersList.FirstOrDefault(_user => _user.Password == Password);
+            if (user == null)
+            {
+                throw new Exception("Invalid Password");
+
+            }
+            return user;
+
+        }
+
+
+    }
+}
